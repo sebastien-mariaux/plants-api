44c047013c418598dd72abebec2ba6e62fa03284
Sébastien Mariaux <sebastien.mariaux@protonmail.com>
2022-05-29 13:26:16 +0200

    rubocop

--- app/models/plant_variety.rb
+++ app/models/plant_variety.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class PlantVariety < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
 end
--- test/models/plant_variety_test.rb
+++ test/models/plant_variety_test.rb
@@ -1,6 +1,8 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PlantVarietyTest < ActiveSupport::TestCase
   should validate_presence_of(:name)
-  
+  should validate_uniqueness_of(:name)
 end
